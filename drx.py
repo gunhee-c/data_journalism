16b618cb0431d6bf6274e432608f441d4cd7669a
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 21:04:48 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -24,7 +24,7 @@ st.subheader("골드")
 data = pd.read_excel("Gold.xlsx")
 data = data.fillna("")
 data.index = data.Time
-data = data.drop(['Time'], axis=1)
+data = data.drop(['Time'], axis=50)
 gold_df = pd.DataFrame(data)
 st.line_chart(gold_df)
 
