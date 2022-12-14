4d81d1ced4da1bbfac7402a0cddb2f56311fc53a
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 18:20:19 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -25,5 +25,4 @@ data = pd.read_excel("Gold.xlsx")
 data = data.fillna("")
 data.index = data.Games
 data = data.drop(["Games"], axis=1)
-gold_data = data[['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48']]
-st.line_chart(gold_data)
+st.write(data.columns)
