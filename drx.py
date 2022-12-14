92bc895fc04f12917ad4805680648468f34155e0
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 23:57:33 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -25,9 +25,9 @@ data = pd.read_excel("Gold.xlsx")
 data = data.fillna("")
 data.index = data.Time
 data = data.drop(['Time'], axis=1)
-#st.dataframe(data)
-gold_data = pd.DataFrame(columns=data.columns[0])
-st.line_chart(gold_data)
+st.dataframe(data)
+#gold_data = pd.DataFrame(columns=data.columns[0])
+#st.line_chart(gold_data)
 
 # install these
 
