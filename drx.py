c05958e8fae62d1fb587aefac31f2545e1a68e8e
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-15 00:24:31 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -31,8 +31,8 @@ for i in range(len(data.columns)):
   gold_columns.append(data.columns[i])
 gold_index = data[data.columns[0]]
 st.write(gold_columns)
-st.write(gold_index)
-st.line_chart(gold_index)
+st.write(gold_index.index[0])
+#st.line_chart(gold_index)
 #for i in range():
   #gold_index[i] = data[
 #option = st.selectbox('Games', gold_columns)
