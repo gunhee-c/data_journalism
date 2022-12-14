3a447e24496e6845c16c3b0e3437cc8304bd4729
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 17:04:39 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -4,8 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.title("2022 LOL 월드 챔피언십 우승 팀 DRX의")
-st.title("경기 데이터 리뷰 및 승리 요인 분석")
-st.write("9조")
+st.title("2022 LOL 월드 챔피언십 우승 팀 DRX의 경기 데이터 리뷰 및 승리 요인 분석")
+st.write("2022 2학기 데이터 저널리즘 과제전 9조")
 st.write("자유전공학부 조건희")
 st.write("언론정보학과 허현준")
