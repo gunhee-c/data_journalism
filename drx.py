419c69c06a9c840fade4252263d0be6bd23ad2e6
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-15 00:10:32 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -29,6 +29,7 @@ st.dataframe(data)
 gold_columns = []
 for i in range(len(data.columns)):
   gold_columns.append(data.columns[i])
+option = st.selectbox('Games', gold_columns)  
 gold_data = pd.DataFrame(np.random.randn(20,21), columns=gold_columns)
 st.line_chart(gold_data)
 
