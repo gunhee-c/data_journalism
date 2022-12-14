145f2994ea9d6e2a5d55f0eb20c1f4bc0ca85903
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 21:18:05 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -25,7 +25,7 @@ data = pd.read_excel("Gold.xlsx")
 data = data.fillna("")
 data.index = data.Time
 data = data.drop(['Time'], axis=1)
-st.dataframe(data)
+st.write(len(data.columns))
 
 # install these
 
