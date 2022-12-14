6cb6fd0574e915a742f4e9cd8d5eb0a9d949b127
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 20:27:32 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -26,9 +26,7 @@ data = data.fillna("")
 gold_data = data.transpose()
 gold_data.rename(columns=gold_data.iloc[0], inplace=True)
 gold_data = gold_data.drop(gold_data.index[0])
-index = gold_data.index
-df1 = pd.DataFrame({range(-20000,20000)}, index=index)
-st.line_chart(df1)
+st.line_chart(gold_data)
 
 # install these
 
