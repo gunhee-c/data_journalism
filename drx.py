940783519eb4963f29fb65adb47a404338619350
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-14 21:13:51 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -25,7 +25,9 @@ data = pd.read_excel("Gold.xlsx")
 data = data.fillna("")
 data.index = data.Time
 data = data.drop(['Time'], axis=1)
-st.dataframe(data)
+#st.dataframe(data)
+line_fig = px.line(data[data.columns[0]], x='Minutes', y='Gold')
+st.plotly_chart(line_fig)
 
 # install these
 
