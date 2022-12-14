993b99988b0da8e6b72e532f0bba226bd928e943
GunHee Cho <114210854+gunhee-c@users.noreply.github.com>
2022-12-15 01:42:44 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import altair as alt
 
 st.title("2022 LOL 월드 챔피언십 우승 팀 DRX의 경기 데이터 리뷰 및 승리 요인 분석")
 st.write("2022-2학기 데이터 저널리즘 과제전 9조")
