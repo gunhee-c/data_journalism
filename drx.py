9bb8851727357d1e93e7119d4fdb6f3195478ac5
darnold934 <120540905+darnold934@users.noreply.github.com>
2022-12-15 00:16:41 +0900

    Update drx.py

--- drx.py
+++ drx.py
@@ -29,11 +29,12 @@ st.dataframe(data)
 gold_columns = []
 for i in range(len(data.columns)):
   gold_columns.append(data.columns[i])
-option = st.selectbox('Games', gold_columns)
-gold_data = data.loc[(gold_columns == option)]
-gold_index = gold_data.index.tolist()
+st.write(gold_columns)
+#option = st.selectbox('Games', gold_columns)
+#gold_data = data.loc[(gold_columns == option)]
+#gold_index = gold_data.index.tolist()
 #gold_data = pd.DataFrame(np.random.randn(20,21), columns=gold_columns)
-st.line_chart(gold_data.loc[gold_index[0]], use_container_width = True)
+#st.line_chart(gold_data.loc[gold_index[0]], use_container_width = True)
 
 # install these
 
